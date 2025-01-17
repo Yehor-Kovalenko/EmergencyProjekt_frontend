2765cc2426ed9dde0e3fbd25825756aa0f3e57f0
freniecki <247773@edu.p.lodz.pl>
2025-01-17 17:42:15 +0100

    connection with backend

--- src/main.js
+++ src/main.js
@@ -3,17 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-// for auth
-import axios from 'axios'
-import { extractIdentifiers } from 'vue/compiler-sfc'
-
-axios.defaults.baseURL = 'http://localhost:8080/api'
-axios.defaults.withCredentials = true
+import axios from './axiosConfig'
 
 const app = createApp(App)
 
-app.use(router)
 app.config.globalProperties.$http = axios
+
+app.use(router)
 app.mount('#app')
-extractIdentifiers
