f38a32985c198e458d21ede1bd743e06c18c40a8
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-01-18 11:54:01 +0100

    invite added

--- src/volounteers.vue
+++ src/volounteers.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,6 +20,11 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/volounteers/1">volounters List</RouterLink>
         <RouterLink to="/volunteers/3/actions/1">test accept</RouterLink>
+        <RouterLink
+          :to="{ name: 'invite', params: { ngoId: '1', eventId: '1001' } }"
+        >
+          test invite
+        </RouterLink>
       </nav>
     </div>
   </header>
