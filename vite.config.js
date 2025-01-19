0efcf9fe388caf5e43fa4d4e8cc3c0dc720824ed
szymonszyman <106918199+szymonszyman@users.noreply.github.com>
2025-01-19 12:41:56 +0100

    Revert "Event2"

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-//import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,11 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-      proxy: {
-          '/api': {
-              target: 'http://localhost:8080', // Adres backendu
-              changeOrigin: true,},
-        },
-    },
-});
+})
