06b8bcf6849b97a6a08e2ebfd7cf97e195ab9b74
freniecki <247773@edu.p.lodz.pl>
2025-01-19 21:16:02 +0100

    path by role - temp

--- src/router/index.js
+++ src/router/index.js
@@ -19,54 +19,62 @@ import MapComponent from '@/components/events/MapComponent.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // ----- example for proper auth ----- 
-    // {
-    //   path: '/path',
-    //   name: 'view',
-    //   component: View,
-    //   meta: {
-    //     requiresAuth: true/false, // requirement of authentication
-           // if requiresAuth is true:
-    //     role: ['GIVER', 'VOLUNTEER', 'OFFICIAL', 'NGO'] // list of roles that have access to this /path
-    //   }
-    // }
     {
       path: "/",
       name: "home",
       component: HomeView,
-      meta: { 
-        requiresAuth: false 
+      meta: {
+        requiresAuth: false,
       }
     },
     {
       path: "/about",
       name: "about",
       component: () => import("../views/AboutView.vue"),
-      meta: { requiresAuth: true }
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthView,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: "/volounteers/:id",
       name: "volounteers",
       component: () => import("../views/volounteersView.vue"),
-      meta: { requiresAuth: true }
+      meta: {
+        requiresAuth: true,
+        role: ['NGO']
+      }
     },
     {
       path: "/mark/:id",
       name: "mark",
       component: () => import("../views/markView.vue"),
-      meta: { requiresAuth: true }
+      meta: {
+        requiresAuth: true,
+        role: ['NGO']
+      }
     },
     {
       path: "/volunteers/:vid/actions/:aid",
       name: "volunteer_action_invite",
       component: () => import("../views/acceptView.vue"),
-      meta: { requiresAuth: true }
+      meta: {
+        requiresAuth: true,
+        role: ['VOLUNTEER']
+      }
     },
     {
       path: "/thanks",
       name: "thank volunteer",
       component: () => import("../views/thanksView.vue"),
-      meta: { 
+      meta: {
         requiresAuth: true,
         role: ['VOLUNTEER']
       }
@@ -75,14 +83,9 @@ const router = createRouter({
       path: "/invite/:ngoId/event/:eventId",
       name: "invite",
       component: () => import("../views/inviteView.vue"),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/auth',
-      name: 'auth',
-      component: AuthView,
-      meta: { 
-        requiresAuth: false 
+      meta: {
+        requiresAuth: true,
+        role: ['VOLUNTEER']
       }
     },
     {
@@ -98,78 +101,127 @@ const router = createRouter({
       path: "/resource/getByholder/:id",
       name: "userResources",
       component: () => import("../views/UserResourcesView.vue"),
+      meta: {
+        requiresAuth: true,
+        role: ['GIVER', 'OFFICIAL', 'NGO']
+      }
     },
     {
       path: "/resource/getBydestination/:id",
       name: "destinationResources",
       component: () => import("../views/ResourcesToCatastropheView.vue"),
+      meta: {
+        requiresAuth: false,
+      }
     },
     {
       path: '/report',
       name: 'report',
       component: ReportView,
-      meta: { requiresAuth: false }
-      // autoryzachje - potem zmienic na true
-
+      meta: {
+        requiresAuth: true,
+        role: ['OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/report-type',
       name: 'report-type',
       component: ReportTypeView,
+      meta: {
+        requiresAuth: true,
+        role: ['OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/report-date',
       name: 'report-date',
       component: ReportDateView,
+      meta: {
+        requiresAuth: true,
+        role: ['OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/report-giver',
       name: 'report-giver',
       component: ReportGiverView,
-      meta: { requiresAuth: false }
-      // autoryzachje - potem zmienic na true
+      meta: {
+        requiresAuth: true,
+        role: ['GIVER', 'NGO']
+      }
     },
     {
       path: '/report-resources-view',
       name: 'resources-view',
       component: ReportResourcesView,
+      meta: {
+        requiresAuth: true,
+        role: ['OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/report-volunteers-view',
       name: 'volunteers-view',
       component: ReportVolunteersView,
+      meta: {
+        requiresAuth: true,
+        role: ['OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/report-catastrophe-view',
       name: 'catastrophe-view',
       component: ReportCatastropheView,
+      meta: {
+        requiresAuth: true,
+        role: ['OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/catastrophes/:catastropheId',
       name: 'CatastropheLookup',
       component: CatastropheLookup,
+      meta: {
+        requiresAuth: true/false,
+        role: ['GIVER', 'VOLUNTEER', 'OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/help-request/edit/:uniqueCode',
       name: 'EditHelpRequest',
       component: EditHelpRequest,
       props: true,
+      meta: {
+        requiresAuth: true/false,
+        role: ['GIVER', 'VOLUNTEER', 'OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/help-request/create/:catastropheId',
       name: 'HelpRequestForm',
       component: HelpRequestForm,
       props: true,
+      meta: {
+        requiresAuth: true/false,
+        role: ['GIVER', 'VOLUNTEER', 'OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/help-request/lookup',
       name: 'HelpRequestLookup',
       component: HelpRequestLookup,
+      meta: {
+        requiresAuth: true/false,
+        role: ['GIVER', 'VOLUNTEER', 'OFFICIAL', 'NGO']
+      }
     },
     {
       path: '/map',
       name: 'MapComponent',
       component: MapComponent,
+      meta: {
+        requiresAuth: false
+      }
     }
   ],
 });
